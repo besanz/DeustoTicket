9fb6e2cc0b6c1b6df6980967ef7d9116dbb3da67
besanz <benat.sanz@opendeusto.es>
2023-04-26 10:35:00 +0200

    Actualizacion de APIS

--- FrontendStaff/src/rmi/server/api/IStaffService.java
+++ FrontendStaff/src/rmi/server/api/IStaffService.java
@@ -3,69 +3,24 @@ package rmi.server.api;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+
+import entidades.Artista;
+import entidades.Evento;
+import entidades.Precio;
+import entidades.Espacio;
 import rmi.server.exceptions.InvalidUser;
 
-import entidades.Staff;
-
 public interface IStaffService extends Remote {
+    String sayHello() throws RemoteException;
+    String sayMessage(String login, String password, String message) throws RemoteException, InvalidUser;
+    void registrarUsuario(String login, String password) throws RemoteException, InvalidUser;
 
-    	/**
-	 * Test message to say hello to client
-	 * @param
-	 * @return Message
-	 * @throws RemoteException
-	 */
-	String sayHello() throws RemoteException;
-	
-	/**
-	 * Print message to client
-	 * @param login
-	 * @param password
-	 * @param message
-	 * @return Message
-	 * @throws RemoteException
-	 */
-	String sayMessage(String login, String password, String message) throws RemoteException, InvalidUser;
-	
-	/**
-	 * Function to register a new user
-	 * @param login
-	 * @param password
-	 * @throws RemoteException
-	 */
-	void registrarUsuario(String login, String password) throws RemoteException, InvalidUser;
+    List<Artista> obtenerArtistas() throws RemoteException;
 
-    /**
-     * Función para crear un nuevo miembro del personal
-     * @param nombre
-     * @param cargo
-     * @return StaffMember
-     * @throws RemoteException
-     */
-    Staff crearStaffMember(String nombre, String cargo) throws RemoteException;
+    List<Evento> obtenerEventos() throws RemoteException;
 
-    /**
-     * Función para obtener la lista de todos los miembros del personal
-     * @return List<StaffMember>
-     * @throws RemoteException
-     */
-    List<Staff> obtenerStaffMembers() throws RemoteException;
+    List<Precio> obtenerPrecios() throws RemoteException;
 
-    /**
-     * Función para actualizar la información de un miembro del personal
-     * @param id
-     * @param nombre
-     * @param cargo
-     * @return StaffMember
-     * @throws RemoteException
-     */
-    Staff actualizarStaffMember(int id, String nombre, String cargo) throws RemoteException;
+    List<Espacio> obtenerEspacios() throws RemoteException;
 
-    /**
-     * Función para eliminar a un miembro del personal
-     * @param id
-     * @return boolean
-     * @throws RemoteException
-     */
-    boolean eliminarStaffMember(int id) throws RemoteException;
 }
