02ef59914892e033acc7a1506213b2bb4ea5bbc4
besanz <benat.sanz@opendeusto.es>
2023-05-02 00:21:39 +0200

    Style + Work Stuff

--- FrontendUser/src/gui/RegisterUser.java
+++ FrontendUser/src/gui/RegisterUser.java
@@ -1,12 +1,14 @@
 package gui;
 
 import controller.UserController;
-import data.entidades.User;
 import remote.ServiceLocator;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+
+import data.entidades.User;
 
 public class RegisterUser extends JFrame {
     private UserController userController;
@@ -25,68 +27,127 @@ public class RegisterUser extends JFrame {
     private void initComponents() {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("GuTicket - Registro de usuario");
+        setBounds(100, 100, 800, 600);
 
-        JPanel contentPane = new JPanel(new GridBagLayout());
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
+        contentPane.setLayout(new BorderLayout());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        mainPanel.setBackground(new Color(54, 57, 63));
+        contentPane.add(mainPanel, BorderLayout.CENTER);
+
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(4, 4, 4, 4);
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        JLabel lblRegistro = new JLabel("Registro - GuTicket");
+        lblRegistro.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 38));
+        lblRegistro.setForeground(new Color(114, 137, 218));
+        GridBagConstraints gbc_lblRegistro = new GridBagConstraints();
+        gbc_lblRegistro.gridx = 0;
+        gbc_lblRegistro.gridy = 0;
+        gbc_lblRegistro.gridwidth = 2;
+        gbc_lblRegistro.anchor = GridBagConstraints.CENTER;
+        gbc_lblRegistro.insets = new Insets(0, 0, 40, 0);
+        mainPanel.add(lblRegistro, gbc_lblRegistro);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(47, 49, 54));
+        panel.setLayout(new GridBagLayout());
+        panel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(new Color(114, 137, 218), 2),
+                BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        GridBagConstraints gbc_panel = new GridBagConstraints();
+        gbc_panel.gridx = 0;
+        gbc_panel.gridy = 1;
+        mainPanel.add(panel, gbc_panel);
 
         // DNI label and text field
         gbc.gridx = 0;
         gbc.gridy = 0;
-        contentPane.add(new JLabel("DNI:"), gbc);
+        JLabel lblDni = new JLabel("DNI:");
+        lblDni.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblDni.setForeground(Color.WHITE);
+        panel.add(lblDni, gbc);
         jTextFieldDNI = new JTextField(20);
+        jTextFieldDNI.setFont(new Font("Tahoma", Font.PLAIN, 16));
         gbc.gridx = 1;
-        contentPane.add(jTextFieldDNI, gbc);
+        panel.add(jTextFieldDNI, gbc);
 
         // Name label and text field
         gbc.gridx = 0;
         gbc.gridy = 1;
-        contentPane.add(new JLabel("Nombre:"), gbc);
+        JLabel lblName = new JLabel("Nombre:");
+        lblName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblName.setForeground(Color.WHITE);
+        panel.add(lblName, gbc);
         jTextFieldName = new JTextField(20);
+        jTextFieldName.setFont(new Font("Tahoma", Font.PLAIN, 16));
         gbc.gridx = 1;
-        contentPane.add(jTextFieldName, gbc);
+        panel.add(jTextFieldName, gbc);
 
         // Surname label and text field
         gbc.gridx = 0;
         gbc.gridy = 2;
-        contentPane.add(new JLabel("Apellidos:"), gbc);
+        JLabel lblSurname = new JLabel("Apellidos:");
+        lblSurname.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblSurname.setForeground(Color.WHITE);
+        panel.add(lblSurname, gbc);
         jTextFieldSurname = new JTextField(20);
+        jTextFieldSurname.setFont(new Font("Tahoma", Font.PLAIN, 16));
         gbc.gridx = 1;
-        contentPane.add(jTextFieldSurname, gbc);
+        panel.add(jTextFieldSurname, gbc);
 
         // Email label and text field
         gbc.gridx = 0;
         gbc.gridy = 3;
-        contentPane.add(new JLabel("Email:"), gbc);
+        JLabel lblEmail = new JLabel("Email:");
+        lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblEmail.setForeground(Color.WHITE);
+        panel.add(lblEmail, gbc);
         jTextFieldEmail = new JTextField(20);
+        jTextFieldEmail.setFont(new Font("Tahoma", Font.PLAIN, 16));
         gbc.gridx = 1;
-        contentPane.add(jTextFieldEmail, gbc);
+        panel.add(jTextFieldEmail, gbc);
 
         // Password label and password field
         gbc.gridx = 0;
         gbc.gridy = 4;
-        contentPane.add(new JLabel("Password:"), gbc);
+        JLabel lblPassword = new JLabel("Password:");
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblPassword.setForeground(Color.WHITE);
+        panel.add(lblPassword, gbc);
         jPasswordField = new JPasswordField(20);
+        jPasswordField.setFont(new Font("Tahoma", Font.PLAIN, 16));
         gbc.gridx = 1;
-        contentPane.add(jPasswordField, gbc);
+        panel.add(jPasswordField, gbc);
 
         // Confirm password label and password field
         gbc.gridx = 0;
         gbc.gridy = 5;
-        contentPane.add(new JLabel("Confirmar password:"), gbc);
+        JLabel lblConfirmPassword = new JLabel("Confirmar password:");
+        lblConfirmPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblConfirmPassword.setForeground(Color.WHITE);
+        panel.add(lblConfirmPassword, gbc);
         jPasswordFieldConfirm = new JPasswordField(20);
+        jPasswordFieldConfirm.setFont(new Font("Tahoma", Font.PLAIN, 16));
         gbc.gridx = 1;
-        contentPane.add(jPasswordFieldConfirm, gbc);
+        panel.add(jPasswordFieldConfirm, gbc);
 
         // Register button
+        JButton jButtonRegister = new JButton("Registrarse");
+        jButtonRegister.addActionListener(evt -> jButtonRegisterActionPerformed(evt));
+        jButtonRegister.setBackground(new Color(114, 137, 218));
+        jButtonRegister.setFont(new Font("Tahoma", Font.BOLD, 16));
+        jButtonRegister.setForeground(Color.WHITE);
         gbc.gridx = 0;
         gbc.gridy = 6;
         gbc.gridwidth = 2;
-        gbc.anchor = GridBagConstraints.CENTER;
-        JButton jButtonRegister = new JButton("Registrarse");
-        jButtonRegister.addActionListener(evt -> jButtonRegisterActionPerformed(evt));
-        contentPane.add(jButtonRegister, gbc);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(jButtonRegister, gbc);
 
         pack();
         setLocationRelativeTo(null);
