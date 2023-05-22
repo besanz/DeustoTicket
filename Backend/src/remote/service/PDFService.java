9e878bfcf0b34d622d4b4d3c638a4947dc43b500
aritznieto <aritz.nieto@opendeusto.es>
2023-05-22 21:05:36 +0200

    pdf tres plantillas

--- Backend/src/remote/service/PDFService.java
+++ Backend/src/remote/service/PDFService.java
@@ -3,24 +3,15 @@ package remote.service;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
 
-import com.google.zxing.*;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import data.entidades.Ticket;
 import remote.service.QRService;
@@ -33,37 +24,61 @@ public class PDFService {
     }
 
     public File generateTicketPDF(Ticket ticket) throws IOException, WriterException {
-        // Create the PDF document
+        // Load the template PDF
         PDDocument document = new PDDocument();
-        PDPage page = new PDPage(PDRectangle.A4);
-        document.addPage(page);
-        String ticketId = String.valueOf(ticket.getId());
+        String evento = ticket.getNombreEvento();
+        System.out.println("Nombre evento: "+ evento);
+        String pdf = null;
+
+        if (evento.equals("Reggaetón Fest")){
+            pdf = "template1.pdf";
+        }else if(evento.equals("EH Fest")){
+            pdf = "template3.pdf";
+        }else if(evento.equals("Latin Fest")){
+            pdf = "template2.pdf";
+        }
+        
+        document = PDDocument.load(new File(pdf));
+        
+        
+        // Get the first page of the loaded template
+        PDPage page = document.getPage(0);
 
         // Generate the QR code
         String qrCodeImagePath = "qrcode.png";
-        qrService.generateQRCodeImage(ticketId, qrCodeImagePath);
+        qrService.generateQRCodeImage(String.valueOf(ticket.getId()), qrCodeImagePath);
 
         // Create the page content stream
-        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true, true);
 
         // Write ticket data to the PDF
+        contentStream.setNonStrokingColor(1f);
         contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-        contentStream.newLineAtOffset(25, 700);
-        contentStream.showText("ID: " + ticket.getId());
-        contentStream.newLineAtOffset(0, -15);
-        contentStream.showText("Evento: " + ticket.getNombreEvento());
-        contentStream.newLineAtOffset(0, -15);
-        contentStream.showText("Precio: " + ticket.getPrecio());
-        contentStream.newLineAtOffset(0, -15);
-        contentStream.showText("Comprado por: " + ticket.getDni());
-        contentStream.newLineAtOffset(0, -15);
+
+        contentStream.setFont(PDType1Font.TIMES_BOLD, 50);
+        contentStream.newLineAtOffset(125, 460);
+        contentStream.showText("" + ticket.getFechaEvento());
+
+        contentStream.setFont(PDType1Font.TIMES_BOLD, 50);
+        contentStream.newLineAtOffset(825, 0);
+        contentStream.showText("" + ticket.getPrecio());
+
+        contentStream.setFont(PDType1Font.TIMES_BOLD, 20);
+        contentStream.newLineAtOffset(-450, -300);
         contentStream.showText("Titular: " + ticket.getTitular());
+
+        contentStream.setFont(PDType1Font.TIMES_BOLD, 20);
+        contentStream.newLineAtOffset(160, 0);
+        contentStream.showText("-");
+
+        contentStream.setFont(PDType1Font.TIMES_BOLD, 20);
+        contentStream.newLineAtOffset(30, 0);
+        contentStream.showText(""+ticket.getDni());
         contentStream.endText();
 
         // Add the QR code to the PDF
         PDImageXObject qrCodeImage = PDImageXObject.createFromFile(qrCodeImagePath, document);
-        contentStream.drawImage(qrCodeImage, 25, 150, 150, 150); // Adjust the coordinates and size according to your needs
+        contentStream.drawImage(qrCodeImage, 500, 375, 200, 200); // Adjust the coordinates and size according to your needs
         contentStream.close();
 
         // Save the PDF document to a file
@@ -74,6 +89,3 @@ public class PDFService {
         return file;
     }
 }
-
-
-
